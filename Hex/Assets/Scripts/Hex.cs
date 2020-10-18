074f84bef1b2a608ef7603023c38d009f4a587b4
anilberg <anilberg@users.noreply.github.com>
2020-10-18 19:15:36 +0300

    Documents

---
+++ Hex/Assets/Scripts/Hex.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex
+{
+    //Q + R + S = 0
+    //S = -(Q + R)
+
+    public readonly int Q;  //col
+    public readonly int R;  //row
+    public readonly int S;
+
+    static readonly float WIDTH_MULTIPLER = Mathf.Sqrt(3) / 2;
+
+    public Hex(int q, int r){
+
+        this.Q = q;
+        this.R = r;
+        this.S = -(q + r);
+
+    }
+
+    //Return the world-space position of tihs hex
+    public Vector3 Position(){
+
+        float radius = 1f;
+        float height = radius * 2;
+        float width = WIDTH_MULTIPLER * height;
+
+        float horiz = width;
+        float vert = height * 0.656f;
+
+        return new Vector3(
+                        horiz * (this.Q + this.R/2f),
+                        0,
+                        vert * this.R);
+    }
+
+}
---
+++ Hex/Assets/Scripts/HexMap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMap();
+        
+    }
+
+    public GameObject HexPreFab;
+
+    public Material[] HexMaterials;
+
+    int mapHeight = 7;
+    int mapWidth = 10;
+
+    public void GenerateMap(){
+
+        for(int column = 0; column < mapWidth; column++){
+            for(int row = 0; row < mapHeight; row++){
+
+                //Instatiate a Hex
+                Hex h = new Hex(column, row);
+                
+                GameObject HexGo = (GameObject)Instantiate(HexPreFab, 
+                            h.Position(),
+                            Quaternion.identity, 
+                            this.transform
+                            );
+
+                HexGo.GetComponentInChildren<TextMesh>().text = string.Format("{0},{1}", column, row);      
+
+                MeshRenderer mr = HexGo.GetComponentInChildren<MeshRenderer>();
+                mr.material = HexMaterials[Random.Range(0, HexMaterials.Length)];
+            }
+        }
+    }
+}
